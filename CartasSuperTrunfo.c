77fbb2aec577c4b2d7a21c4dd222d747168df951
kayBCC <kaylanefernandes695@gmail.com>
2025-04-09 14:49:32 +0000

    criando o jogo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,6 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
-    return 0;
-}
+    printf("desafio super trunfo!\n");
+    printf("novo commit\n");
+
